457bce2ab169d5430682188bbeb69c8f457918c4
ArthurDem <arthurdemagny@yahoo.fr>
2019-02-22 14:03:42 +0100

    Jest test completed

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -6,26 +6,31 @@ describe('starwars-names', () => {
     test('should be a fulfilled array', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
-    test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    test('should be an array of strings', () => { 
+      var test = ""
+      starWars.all.forEach(element => {
+        expect(typeof element).toBe(typeof test)
+          
+        });
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    expect(starWars.all).toContain("Luke Skywalker")
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain("Ben Quadinaros")
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain(starWars.random())
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      var test = 2
+      expect(starWars.all).not.toContain(starWars.random(test))
     });
   });
 });
