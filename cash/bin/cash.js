84152be92ef36179a5afa92e16fac6ae93bd74ac
ArthurDem <arthurdemagny@yahoo.fr>
2019-02-22 11:19:39 +0100

    JSON block completed

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -7,13 +7,20 @@ const ora = require('ora');
 const currencies = require('../lib/currencies.json');
 
 const {API} = require('./constants');
-
+/**
+ * Convert a certain amount to another currency
+ * @param {int} command 
+ */
 const cash = async command => {
 	const {amount} = command;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
 
 	console.log();
+	/**
+	 * Render of a spinner
+	 * @function ora 
+	 */
 	const loading = ora({
 		text: 'Converting...',
 		color: 'green',
@@ -22,15 +29,16 @@ const cash = async command => {
 			frames: to
 		}
 	});
-
+	/**Runs the spinner */
 	loading.start();
 
+	/**Converts using the bases and rates of the currency chosen */
 	await got(API, {
 		json: true
 	}).then(response => {
 		money.base = response.body.base;
 		money.rates = response.body.rates;
-
+		/** For each @param {int} item in the table @constant currencies it converts the amount from one currency to another*/
 		to.forEach(item => {
 			if (currencies[item]) {
 				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`);
